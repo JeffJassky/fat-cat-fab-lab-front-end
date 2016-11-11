b02a4f70cf9ba717e825052f3d964d71a44f940a
Jeff Jassky <jeff@jeffjassky.com>
2016-11-10 19:40:30 -0500

    Issue #2 - Fixed nested for loops variables by replacing them with _.each methods

--- js/Entities/Course/CourseModel.js
+++ js/Entities/Course/CourseModel.js
@@ -10,17 +10,30 @@ App.module('Entities.Course', function(Course){
         response.mainPhoto = response.photos[0];
         delete response.photos[0];
       }
-      if(response.sessions){
-        for(var i=0; i < response.sessions.length; i++){
-          response.sessions[i].startDateNice = dateHelper.dateNice(new Date(response.sessions[i].start_date));
-          response.sessions[i].startTimeNice = dateHelper.timeOfDay(new Date(response.sessions[i].start_date));
-        }
-      }
       if(response.startDates){
-        for(var i=0; i < response.startDates.length; i++){
-          response.startDates[i].startDateNice = dateHelper.dateNice(new Date(response.startDates[i].start_date));
-          response.startDates[i].startTimeNice = dateHelper.timeOfDay(new Date(response.startDates[i].start_date));
-        }
+
+        // FILTER OUT ALL UNUSED KEYS FOR START DATES
+        response.startDates = _.map(response.startDates, function(startDate){
+          return {
+            start_date: startDate.start_date,
+            end_date: startDate.end_date,
+            registration_url: startDate.registration_url,
+            sessions: startDate.sessions
+          };
+        });
+
+        // FORMAT START DATE STRINGS TO BE PRETTIER
+        response.startDates = _.each(response.startDates, function(startDate){
+          startDate.startDateNice = dateHelper.dateNice(new Date(startDate.start_date));
+          startDate.startTimeNice = dateHelper.timeOfDay(new Date(startDate.start_date));
+          if(startDate.sessions){
+            // FORMAT SESSION DATE STRINGS TO BE PRETTIER
+            startDate.sessions = _.each(startDate.sessions, function(session){
+              session.startDateNice = dateHelper.dateNice(new Date(session.start_date));
+              session.startTimeNice = dateHelper.timeOfDay(new Date(session.start_date));
+            });
+          }
+        });
       }
       response.skillLevel = response.level ? skillLevels[response.level -1] : skillLevels[0];
       return response;
